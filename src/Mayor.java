bcc56fbaa27544daa0e5e8141f1c3f6846f2abb2
alu10513774 <alucor3774@ieselcaminas.org>
2021-11-16 17:31:45 +0100

    Initial Commit

---
+++ src/Mayor.java
@@ -0,0 +1,17 @@
+public class Mayor {
+
+    public static void Mayor(String edad) {
+
+        System.out.println(edad);
+    }
+
+    public static void main(String[] args) {
+        int edad = 18;
+        if (edad >= 18) {
+            Mayor("Mayor de edad");
+
+        }
+    }
+}
+
+
